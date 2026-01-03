3b27b309a9a90113daeba1be6697b62ebad37e80
Edgardo <edgardo.flores@outlook.com>
2026-01-02 21:48:09 -0800

    Se agrega ejercicio de arrays

---
+++ arrays.js
@@ -0,0 +1,14 @@
+console.log("===Array (arreglos/matrices)===")
+
+let frutas = ["manzana", "banana", "cereza"]; //array de frutas
+console.log("frutas disponibles",frutas);
+console.log("Tercera fruta",frutas[2]);//Accesoal tercer elemento
+
+
+let numeros = [10, 20, 30, 40, 50]; //Arrays de números
+console.log("Numeros disponibles", numeros);
+console.log("Primer número", numeros[0]); //Acceso al primer elemento
+
+let mezclado = ["texto", 100, true, 3.14]; //Array con tipos mixtos
+console.log("Array mezclado:", mezclado);
+console.log("Segundo elemento del array mezclado", mezclado[1]); //Acceso al segundo elemento
