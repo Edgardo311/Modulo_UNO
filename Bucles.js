793026e535332965248389560ee76f28c7f2460e
Edgardo <edgardo.flores@outlook.com>
2026-01-23 20:44:20 -0800

    ejercicios 4 y  arrays

--- Bucles.js
+++ Bucles.js
@@ -16,3 +16,26 @@ for  (let i = 1; i <= 10; i+=5) {
 
 
 console.log("Fin de ejercicio 4");
+
+console.log("===Ejericicio 4: arrays/arreglos===")
+let frutas = ["Manzanas", "Bananas", "cerezas", "duraznos", "uvas"];
+console.log("frutas ", frutas)
+console.log("Cantidad de frutas: ", frutas.lenght);
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log("Frutas en posición", i + ":", frutas[i]);
+}
+
+console.log("Fin de ejercicio 4 arrays/arreglos");
+
+console.log("===Ejericicio 5: arrays/arreglos con modificación===")
+frutas = ["Manzanas", "Bananas"];
+console.log("frutas ", frutas)
+console.log("Cantidad de frutas: ", frutas.lenght);
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log("Frutas en posición", i + ":", frutas[i]);
+}
+
+console.log("Fin de ejercicio 5: arrays/arreglos con modificación");
+
