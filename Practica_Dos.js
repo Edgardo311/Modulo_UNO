92644b78d3d23b7fa2841fe525b46721a36de8d0
Edgardo <edgardo.flores@outlook.com>
2026-01-23 22:50:25 -0800

    Actualizacion

--- Practica_Dos.js
+++ Practica_Dos.js
@@ -15,17 +15,45 @@ CalificacionesDos [85, 90, 78];
 CalificacionesTres [65, 70, 72, 68, 60];
 */
 
-let resultado = 0;
-let promedio = 0;
 
 
-let calificacionesTres = [65, 70, 72, 68, 60];
+
+let calificacionesTres = [85, 90, 78];
 console.log("Calificaciones: ", calificacionesTres);
+
+let resultado = 0;
+let promedio;
 
 
 for (let i = 0; i < calificacionesTres.length; i++) {
+    //primera vez de i = 0; resultado = 0 + calificación[0];
+    //segunda vez de i = 1; resultado = resultado(65) + calificación[1](70); = 135
+    //tercera vez de i = 2; resultado = resultado(135) + calificación[2](72);
+    //cuarta vez de i = 3; resultado = resultado(207) + calificación[3](68);
+    //cuarta vez de i = 4; resultado = resultado(275) + calificación[4](60); 
+    console.log("CalificacionesTres:", i);
+
     resultado = resultado + calificacionesTres[i];
+    
 }
 
+console.log("Suma de calificaciones: ", resultado);
+
+Promedio = resultado / calificacionesTres.length;
+
 // Todo: Muestra un Mensaje "Mi promedio es: [resultado]"
-console.log(`Mi promedio es ", ${resultado}`);
+console.log(`Mi promedio es ", ${Promedio}`);
+
+const calificacion = promedio; 
+let parametro;
+
+switch (calificacion){
+    case 1:
+        calificacion = promedio > 80, "Lacalificacion es A";
+        break;
+    default:
+        nombreDia = "Dia invalido"
+}
+
+console.log("Calificacion es: ", calificacion);
+console.log("Fin de ejercicio 1")
