e51c5fb2427a0cd56c356ca71ac1b6a978df1edd
Edgardo <edgardo.flores@outlook.com>
2026-01-23 18:40:55 -0800

    Actualización

--- Bucles.js
+++ Bucles.js
@@ -1,15 +1,1 @@
-//Puede votar
 
-const edadVotante = 20;
-
-if (edadVotante >= 18) {
-
-    console.log("Puede votar")
-
-}
-
-else 
-{
-
-    console.log("No puede votar")    
-}
---
+++ Condicionales.js
@@ -0,0 +1,15 @@
+//Puede votar
+
+const edadVotante = 20;
+
+if (edadVotante >= 18) {
+
+    console.log("Puede votar")
+
+}
+
+else 
+{
+
+    console.log("No puede votar")    
+}
