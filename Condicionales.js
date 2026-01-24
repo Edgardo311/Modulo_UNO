f5c775e2092f8bd40bf5cd1b226e8bf99f092f93
Edgardo <edgardo.flores@outlook.com>
2026-01-23 19:29:51 -0800

    Adición de ejercicios

--- Condicionales.js
+++ Condicionales.js
@@ -1,19 +1,15 @@
+//Ejercicio 1 Condicionales
+console.log("===Ejercicio 1: condicionales====")
 //Puede votar
 
 const edadVotante = 17;
 
 if (edadVotante >= 18) {
-
     console.log("Puede votar")
-
 }
-
-else 
-{
-
+else {
     console.log("No puede votar")    
 }
-
 console.log("===Fin del ejercicio 1===");
 
 //Ejercicio 2 Condicionales
@@ -33,3 +29,31 @@ if (calificacion >= 90) {
 
 console.log("Fin de ejercicio 2");
 
+
+//Ejercicio 3.1 Condicionales multiples "&&"
+console.log("===Condicionales multiples &&AND===")
+//Puedo hacer servicio militar
+const edadCadete = 16;
+const esHombre = true;
+
+if (edadCadete >= 18 && esHombre) {
+    console.log("Puede hacer servicio militar")
+}
+else {
+    console.log("No puede hacer servicio militar")    
+}
+console.log("===Fin del ejercicio 3.1===");
+
+//Ejercicio 3.2 Condicionales multiples "||OR"
+console.log("===Condicionales multiples ||OR===")
+//Puedo hacer servicio militar
+const edadCadete1 = 16;
+const esHombre1 = true;
+
+if (edadCadete1 >= 18 || esHombre1) {
+    console.log("Puede hacer servicio militar")
+}
+else {
+    console.log("No puede hacer servicio militar")    
+}
+console.log("===Fin del ejercicio 3.2===");
