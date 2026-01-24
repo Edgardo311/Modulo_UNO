0993ee45b844a611096f6c0c055d3924e56d8d6a
Edgardo <edgardo.flores@outlook.com>
2026-01-23 20:27:19 -0800

    Ejercicio 1

--- Bucles.js
+++ Bucles.js
@@ -1,1 +1,18 @@
+//Bucles
+console.log("===Ejericicio 4: for===")
 
+// let i = 1; inicio
+// i <= 5; Condición de continuación 
+// i++ Incremento
+
+
+for  (let i = 1; i <= 10; i++) {
+    console.log("Números:", i);
+}
+
+for  (let i = 1; i <= 10; i+=5) {
+    console.log("Números:", i);
+}
+
+
+console.log("Fin de ejercicio 4");
