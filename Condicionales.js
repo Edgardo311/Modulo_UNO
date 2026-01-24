d0e1ee21b4312d3a693d312a5d5e742c605cab08
Edgardo <edgardo.flores@outlook.com>
2026-01-23 19:09:01 -0800

    Ejercicios 1 y 2

--- Condicionales.js
+++ Condicionales.js
@@ -13,3 +13,23 @@ else
 
     console.log("No puede votar")    
 }
+
+console.log("===Fin del ejercicio 1===");
+
+//Ejercicio 2 Condicionales
+console.log("===Ejercicio 2: Condicionales===")
+
+let calificacion = 0;
+
+if (calificacion >= 90) {
+    console.log("Excelente")
+} else if (calificacion >= 80){
+    console.log("Aprobado")
+} else if (calificacion <=70) {
+    console.log("Reprobado")
+} else {
+    console.log("Invalida")
+}
+
+console.log("Fin de ejercicio 2");
+
