c551f933a436e4bd0c314f1d13ce33721a3b5233
Edgardo <edgardo.flores@outlook.com>
2026-01-23 21:14:18 -0800

    Ejericicio

---
+++ Practica_Dos.js
@@ -0,0 +1,23 @@
+// =======Reto final=======
+console.log("===Reto Final===");
+/* Todo: Crea un programa que calcule el promedio de las calificaciones dadas con arreglo
+de calificaciones y muestre el resultado en consola donde 
+A = 90- 100;
+B = 80 - 89;
+C = 70- 79;
+D = 60- 69;
+F = 0- 59;
+
+
+Arreglos
+CalificacionesUno [85, 90, 78, 92, 88];
+CalificacionesDos [85, 90, 78];
+CalificacionesTres [65, 70, 72, 68, 60];
+*/
+
+let calificaciones = [65, 70, 72, 68, 60];
+console.log("Calificaciones:", calificaciones);
+let resultado = 0;
+
+// Todo: Muestra un Mensaje "Mi promedio es: [resultado]"
+console.log(`Mi promedio es ", ${resultado}`);
