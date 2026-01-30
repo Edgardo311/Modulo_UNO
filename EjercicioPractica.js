132a3d1fdb67256c41de250e40abe1d9fd3a9a0c
Edgardo <edgardo.flores@outlook.com>
2026-01-29 18:50:02 -0800

    Actualizacion

--- EjercicioPractica.js
+++ EjercicioPractica.js
@@ -19,4 +19,41 @@ Tip: Usa un bucle para recorrer el arreglo y condicionales para clasificar los n
 Ejemplo de salida esperada:
 Positiva: 3
 Negativa: 2
-Caros: 2
+Ceros: 2 
+*/
+
+//Crea un programa que recorra un arreglo de numeros y determine
+
+const numerosUno = [5, -3, 0, 12 -7, 0, 9];
+const numerosDos = [0, 0, 0, 4, 8];
+const numerosTres = [-1, -5, -10, 3, 6];
+
+console.log("Numeros: ", numerosUno);
+
+// variables de referencia:
+
+let cero = 0;
+let positivo = 0;
+let negativo = 0;
+
+
+//Determina cuantos valores son positivos y negativos:
+
+for ( let i = 0; i < numerosUno.length; i++){
+    console.log("numerosUno:", i);
+
+    if(numerosUno[i] == 0)
+    cero = cero + 1; 
+
+    else if(numerosUno[i] > 0)
+    positivo = positivo + 1; 
+
+    else if(numerosUno[i] < 0)
+    negativo = negativo + 1; 
+
+    }
+
+
+console.log("Ceros: ", cero);
+console.log("Positivos: ", positivo);
+console.log("Positivos: ", negativo);
